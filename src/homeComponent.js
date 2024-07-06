ce5693eb0537d10860bda628e0f2bfcebc526014
SpaceWaffle42 <86594101+SpaceWaffle42@users.noreply.github.com>
2024-07-06 13:13:18 +0100

    removed

--- src/homeComponent.js
+++
@@ -1,8 +0,0 @@
-// Component 1: Home Component
-function homeComponent(req, res) {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello from the Home Component!\n');
-}
-
-module.exports = homeComponent;
